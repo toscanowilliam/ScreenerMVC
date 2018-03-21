4a938a49942c459898a8109f3f13bc0dbe52df9a
william <william_toscano_@hotmail.com>
2018-03-21 16:51:31 -0700

    inital commit

---
+++ src/main/java/com/example/scrennerMVC/ScrennerMvcApplication.java
@@ -0,0 +1,12 @@
+package com.example.scrennerMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrennerMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrennerMvcApplication.class, args);
+	}
+}
